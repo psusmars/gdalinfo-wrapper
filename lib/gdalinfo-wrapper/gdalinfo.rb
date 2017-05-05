ed832cd9e76aa47f959eeb94f12dee201d047be5
Paul Susmarski <Paul.susmarski@gmail.com>
2017-05-05 10:23:45 -0600

    fix the end statements and the whitespace

--- lib/gdalinfo-wrapper/gdalinfo.rb
+++ lib/gdalinfo-wrapper/gdalinfo.rb
@@ -52,65 +52,64 @@ module GDALInfoWrapper
           j = i + 1
           j += 1 while j < @raw_gdal_info.length && !(/Band \d+/ =~ @raw_gdal_info[j])
 
-                  # band line
-                  m = /(Band \d+).+?Type=(.+?),\s*ColorInterp=(\S+)/.match(line)
-                  band_variable_name = Util.underscore(m[1]).gsub("_", "")
-                  self.class.send(:attr_accessor, band_variable_name)
-                  band = Band.new()
-                  band.type = m[2]
-                  band.color_interp = m[3]
+          # band line
+          m = /(Band \d+).+?Type=(.+?),\s*ColorInterp=(\S+)/.match(line)
+          band_variable_name = Util.underscore(m[1]).gsub("_", "")
+          self.class.send(:attr_accessor, band_variable_name)
+          band = Band.new()
+          band.type = m[2]
+          band.color_interp = m[3]
 
-                  # We now have our array for our band
-                  while i < j do
-                    line = @raw_gdal_info[i]
-                    case line
-                    when /\A(\d|\s)+\Z/
-                          # just buckets
-                          band.buckets = @raw_gdal_info[i].split(" ").map(&:to_i)
-                        when /Overviews: (.+?)/
-                          band.overviews = $~[1]
-                        when /Metadata:/
-                          i += 1 
-                          while m = Util::METADATA_REGEX.match(@raw_gdal_info[i]) do
-                            band.metadata[m[1]] = m[2]
-                            i += 1
-                          end
-                          i -= 1
-                        end
-                        i += 1
-                      end
-                      i -= 1
-                      instance_variable_set("@#{band_variable_name}", band)
-                    end
-                    i += 1 
+          # We now have our array for our band
+          while i < j do
+            line = @raw_gdal_info[i]
+            case line
+            when /\A(\d|\s)+\Z/
+                  # just buckets
+                  band.buckets = @raw_gdal_info[i].split(" ").map(&:to_i)
+                when /Overviews: (.+?)/
+                  band.overviews = $~[1]
+                when /Metadata:/
+                  i += 1 
+                  while m = Util::METADATA_REGEX.match(@raw_gdal_info[i]) do
+                    band.metadata[m[1]] = m[2]
+                    i += 1
                   end
+                  i -= 1
                 end
-
-                def to_s
-                  return self.raw_gdal_info.join("\n")
-                end
-
-                def all_bands_have_overviews?
-                  overviews = false
-                  i = 1
-                  while b = self.instance_variable_get("@band#{i}") do
-              # We at least have bands
-              overviews = true if i == 1
-              overviews = overviews && b.has_overviews?
-              i += 1
+                i += 1
+              end
+              i -= 1
+              instance_variable_set("@#{band_variable_name}", band)
             end
-            return overviews
-          end
-
-          def is_image_projection?(projection)
-            m =  /PROJCS/.match(self.coordinate_system)
-            if projection.nil? || projection.empty? then
-              return !m
-            else
-              return !!m
-            end
+            i += 1 
           end
         end
+      end
+    end
+
+    def to_s
+      return self.raw_gdal_info.join("\n")
+    end
+
+    def all_bands_have_overviews?
+      overviews = false
+      i = 1
+      while b = self.instance_variable_get("@band#{i}") do
+        # We at least have bands
+        overviews = true if i == 1
+        overviews = overviews && b.has_overviews?
+        i += 1
+      end
+      return overviews
+    end
+
+    def is_image_projection?(projection)
+      m =  /PROJCS/.match(self.coordinate_system)
+      if projection.nil? || projection.empty? then
+        return !m
+      else
+        return !!m
       end
     end
   end
